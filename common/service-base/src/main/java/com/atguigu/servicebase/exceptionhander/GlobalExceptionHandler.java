c9d301ad9079f4db8424438039edba7c4d85a193
SongYu-max <1390493172@qq.com>
2022-03-26 19:29:27 +0800

    完成视频至P35-统一日志处理 ps:同视频一样，没有创建日志工具类

--- common/service-base/src/main/java/com/atguigu/servicebase/exceptionhander/GlobalExceptionHandler.java
+++ common/service-base/src/main/java/com/atguigu/servicebase/exceptionhander/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.atguigu.servicebase.exceptionhander;
 
 import com.atguigu.commonutils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @Description TODO
  * @createTime 2022年03月26日 14:28:00
  */
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -25,6 +27,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(GuliException.class)
     @ResponseBody //为了返回数据
     public R error(GuliException e) {
+        log.error(e.getMessage());
         e.printStackTrace();
         return R.error().code(e.getCode()).message(e.getMsg());
     }
