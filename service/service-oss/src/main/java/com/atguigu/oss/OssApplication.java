6aec55a3c4728faa9a1c1181e0809728cdff094e
SongYu-max <1390493172@qq.com>
2022-03-31 15:11:48 +0800

    完成视频至P90-上传讲师头像

---
+++ service/service-oss/src/main/java/com/atguigu/oss/OssApplication.java
@@ -0,0 +1,21 @@
+package com.atguigu.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author SongYu
+ * @version 1.0.0
+ * @ClassName OssApplication.java
+ * @Description TODO
+ * @createTime 2022年03月31日 14:03:00
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.atguigu"})
+public class OssApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OssApplication.class,args);
+    }
+}
