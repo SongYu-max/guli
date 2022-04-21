5a4c5415663f21b36d3fa72991cbc5933dd108b1
SongYu-max <1390493172@qq.com>
2022-04-21 23:42:33 +0800

    完成视频至P185

---
+++ service/service-msm/src/main/java/com/atguigu/msmservice/MsmApplication.java
@@ -0,0 +1,21 @@
+package com.atguigu.msmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author SongYu
+ * @version 1.0.0
+ * @ClassName MsmApplication.java
+ * @Description TODO
+ * @createTime 2022年04月21日 22:09:00
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan("com.atguigu")
+public class MsmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsmApplication.class,args);
+    }
+}
