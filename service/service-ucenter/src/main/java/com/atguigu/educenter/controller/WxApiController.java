6dd78cc692a6e5cb2610c8d31057dd42d50784ea
SongYu-max <1390493172@qq.com>
2022-04-25 23:19:19 +0800

    完成视频至P201

---
+++ service/service-ucenter/src/main/java/com/atguigu/educenter/controller/WxApiController.java
@@ -0,0 +1,56 @@
+package com.atguigu.educenter.controller;
+
+import com.atguigu.educenter.utils.ConstantWxUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URLEncoder;
+
+/**
+ * @author SongYu
+ * @version 1.0.0
+ * @ClassName WxApiController.java
+ * @Description TODO
+ * @createTime 2022年04月25日 09:56:00
+ */
+@Controller
+@RequestMapping("/api/ucenter/wx")
+@CrossOrigin
+public class WxApiController {
+    //1 生成微信扫描二维码
+    @GetMapping("login")
+    public String getWxCode() {
+        //固定地址，后面拼接参数
+//        String url = "https://open.weixin.qq.com/" +
+//                "connect/qrconnect?appid="+ ConstantWxUtils.WX_OPEN_APP_ID+"&response_type=code";
+
+        // 微信开放平台授权baseUrl  %s相当于?代表占位符
+        String baseUrl = "https://open.weixin.qq.com/connect/qrconnect" +
+                "?appid=%s" +
+                "&redirect_uri=%s" +
+                "&response_type=code" +
+                "&scope=snsapi_login" +
+                "&state=%s" +
+                "#wechat_redirect";
+
+        //对redirect_url进行URLEncoder编码
+        String redirectUrl = ConstantWxUtils.WX_OPEN_REDIRECT_URL;
+        try {
+            redirectUrl = URLEncoder.encode(redirectUrl, "utf-8");
+        }catch(Exception e) {
+        }
+
+        //设置%s里面值
+        String url = String.format(
+                baseUrl,
+                ConstantWxUtils.WX_OPEN_APP_ID,
+                redirectUrl,
+                "atguigu"
+        );
+
+        //重定向到请求微信地址里面
+        return "redirect:"+url;
+    }
+}
