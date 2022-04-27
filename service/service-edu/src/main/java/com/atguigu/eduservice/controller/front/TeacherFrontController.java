be1a733c235ec8a693e4fc9f9bc36b3c195805e8
SongYu-max <1390493172@qq.com>
2022-04-27 23:55:04 +0800

    完成视频至P207

---
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/front/TeacherFrontController.java
@@ -0,0 +1,34 @@
+package com.atguigu.eduservice.controller.front;
+
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author SongYu
+ * @version 1.0.0
+ * @ClassName TeacherFrontController.java
+ * @Description TODO
+ * @createTime 2022年04月27日 22:09:00
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/eduservice/teacherfront")
+public class TeacherFrontController {
+    @Autowired
+    private EduTeacherService eduTeacherService;
+
+    //分页查询讲师
+    @PostMapping("getTeacherFrontList/{page}/{limit}")
+    public R getTeacherFrontList(@PathVariable long page, @PathVariable long limit) {
+        Page<EduTeacher> pageTeacher = new Page<EduTeacher>(page,limit);
+        Map<String, Object> map = eduTeacherService.getTeacherFrontList(pageTeacher);
+        return R.ok().data(map);
+    }
+}
--- service/service-edu/src/main/java/com/atguigu/eduservice/service/impl/EduTeacherServiceImpl.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/service/impl/EduTeacherServiceImpl.java
@@ -3,8 +3,15 @@ package com.atguigu.eduservice.service.impl;
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.mapper.EduTeacherMapper;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -17,4 +24,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
 
+    @Override
+    public Map<String, Object> getTeacherFrontList(Page<EduTeacher> pageTeacher) {
+        QueryWrapper<EduTeacher> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("id");
+        baseMapper.selectPage(pageTeacher, queryWrapper);
+//        this.page(pageTeacher,queryWrapper);//todo 这里的这个this指的是什么
+
+        //把分页数据放到map集合中
+        List<EduTeacher> records = pageTeacher.getRecords();
+        long current = pageTeacher.getCurrent();
+        long pages = pageTeacher.getPages();
+        long size = pageTeacher.getSize();
+        long total = pageTeacher.getTotal();
+        boolean hasNext = pageTeacher.hasNext();
+        boolean hasPrevious = pageTeacher.hasPrevious();
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("records",records);
+        map.put("current",current);
+        map.put("pages",pages);
+        map.put("size",size);
+        map.put("total",total);
+        map.put("hasNext",hasNext);
+        map.put("hasPrevious",hasPrevious);
+        return map;
+    }
 }
