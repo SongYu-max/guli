9d0eb83e79a232e501a6805f015ca983c9aa7100
SongYu-max <1390493172@qq.com>
2022-05-18 23:42:40 +0800

    完成视频至P251

--- service/service-statistics/src/main/java/com/atguigu/staservice/controller/StatisticsDailyController.java
+++ service/service-statistics/src/main/java/com/atguigu/staservice/controller/StatisticsDailyController.java
@@ -6,6 +6,8 @@ import com.atguigu.staservice.service.StatisticsDailyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 /**
  * <p>
  * 网站统计日数据 前端控制器
@@ -27,6 +29,12 @@ public class StatisticsDailyController {
         statisticsDailyService.registerCount(day);
         return R.ok();
     }
-
+    //图表显示，返回两部分数据，日期json数组，数量json数组
+    @GetMapping("showData/{type}/{begin}/{end}")
+    public R showData(@PathVariable String type,@PathVariable String begin,
+                      @PathVariable String end) {
+        Map<String,Object> map = statisticsDailyService.getShowData(type,begin,end);
+        return R.ok().data(map);
+    }
 }
 
--- service/service-statistics/src/main/java/com/atguigu/staservice/service/impl/StatisticsDailyServiceImpl.java
+++ service/service-statistics/src/main/java/com/atguigu/staservice/service/impl/StatisticsDailyServiceImpl.java
@@ -11,6 +11,11 @@ import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * <p>
  * 网站统计日数据 服务实现类
@@ -44,5 +49,50 @@ public class StatisticsDailyServiceImpl extends ServiceImpl<StatisticsDailyMappe
         statisticsDaily.setLoginNum(RandomUtils.nextInt(100,200));
         statisticsDaily.setCourseNum(RandomUtils.nextInt(100,200));
         baseMapper.insert(statisticsDaily);
+    }
+
+    //图表显示，返回两部分数据，日期json数组，数量json数组
+    @Override
+    public Map<String, Object> getShowData(String type, String begin, String end) {
+        //根据条件查询对应数据
+        QueryWrapper<StatisticsDaily> wrapper = new QueryWrapper<>();
+        wrapper.between("date_calculated",begin,end);
+        wrapper.select("date_calculated",type);
+        List<StatisticsDaily> staList = baseMapper.selectList(wrapper);
+
+        //因为返回有两部分数据：日期 和 日期对应数量
+        //前端要求数组json结构，对应后端java代码是list集合
+        //创建两个list集合，一个日期list，一个数量list
+        List<String> date_calculatedList = new ArrayList<>();
+        List<Integer> numDataList = new ArrayList<>();
+
+        //遍历查询所有数据list集合，进行封装
+        for (int i = 0; i < staList.size(); i++) {
+            StatisticsDaily daily = staList.get(i);
+            //封装日期list集合
+            date_calculatedList.add(daily.getDateCalculated());
+            //封装对应数量
+            switch (type) {
+                case "login_num":
+                    numDataList.add(daily.getLoginNum());
+                    break;
+                case "register_num":
+                    numDataList.add(daily.getRegisterNum());
+                    break;
+                case "video_view_num":
+                    numDataList.add(daily.getVideoViewNum());
+                    break;
+                case "course_num":
+                    numDataList.add(daily.getCourseNum());
+                    break;
+                default:
+                    break;
+            }
+        }
+        //把封装之后两个list集合放到map集合，进行返回
+        Map<String, Object> map = new HashMap<>();
+        map.put("date_calculatedList",date_calculatedList);
+        map.put("numDataList",numDataList);
+        return map;
     }
 }
